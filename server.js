4eb10f2c090550f51e0672dd524c1285a6fbf338
MagnusRox <vikashram.09@gmail.com>
2020-07-14 09:16:10 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,19 +1,3 @@
-//var http = require('http');
-//var fs = require('fs');
-//var port = process.env.PORT || 3338;
-
-//http.createServer(function (req, res) {
-//    res.writeHead(200, { 'Content-Type':'text/html' });
-//    fs.readFile('./index.html', null,function(error,data) {
-//        if (error) {
-//            res.writeHead(404);
-//            res.write('html file not found');
-//        } else {
-//            res.write(data);
-//        }+
-//        res.end();
-//    });
-//}).listen(port);
 var express = require("express");
 var app = express();
 var path = require("path");
@@ -21,5 +5,18 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
     //__dirname : It will resolve to your project folder.
 });
+app.get('/snakey.js', function (req, res) {
+    res.sendFile(path.join(__dirname + '/snakey.js'));
+});
+app.get('/style.css', function (req, res) {
+    res.sendFile(path.join(__dirname + '/style.css'));
+});
+app.get('/food.png', function (req, res) {
+    res.sendFile(path.join(__dirname + '/food.png'));
+});
+app.get('/ground.png', function (req, res) {
+    res.sendFile(path.join(__dirname + '/ground.png'));
+});
 app.listen(3000);
 console.log("Server running at Port 3000");
+
